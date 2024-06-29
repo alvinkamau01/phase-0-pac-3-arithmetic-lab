0f663670cdfd8d378602debc3dd5049312bccea2
Alvin Kamau <alvin.kamau@student.moringaschool.com>
2024-06-29 10:48:42 +0300

    Assignments

---
+++ index.js
@@ -0,0 +1,39 @@
+
+function add(a,b) {
+    return  a+b
+}
+
+function subtract(a,b) {
+    return  a-b
+} 
+
+function multiply(a,b) {
+    return  a*b
+} 
+
+function divide(a,b) {
+    return  a/b
+}
+
+function increment(n) {
+    return ++n
+}
+
+function decrement(n) {
+    return --n
+}
+
+
+function makeInt(string){
+   parseInt("string",10)
+    return string
+}
+
+makeInt("n")
+
+function preserveDecimal(string){
+    parseFloat("string")
+     return string
+ }
+ 
+ preserveDecimal("n")
